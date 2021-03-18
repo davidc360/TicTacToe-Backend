2925cb760a3074b1632cd5a6b46f422bb36ba371
David Cao <davidcao@Davids-MBP.fios-router.home>
2021-03-18 18:46:09 -0400

    return error if game not found

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from flask_cors import CORS
 
 from flask_pymongo import PyMongo
 
 import json
+import uuid
 import os
 from dotenv import load_dotenv, find_dotenv
 
@@ -15,14 +16,44 @@ import json
 app = Flask(__name__)
 CORS(app)
 # mongo URI for pymongo
-# MONGO_URI = os.environ.get("MONGO_URI")
-# app.config["MONGO_URI"] = MONGO_URI
-# mongo = PyMongo(app)
+MONGO_URI = os.environ.get("MONGO_URI")
+app.config["MONGO_URI"] = MONGO_URI
+mongo = PyMongo(app)
+gamesdb = mongo.db.games
 
 # home route
 @app.route('/')
 def home():
     return "678345012"
+
+@app.route('/game', methods=['POST'])
+def post_game():
+    game_id = str(uuid.uuid4())[:8]
+    while gamesdb.find_one({'id': game_id}) is not None:
+        game_id = str(uuid.uuid4())[:8]
+    params = request.get_json()
+    gamesdb.insert_one({
+        "id": game_id,
+        "n": params.get('name'),
+        "m": params.get('moves')
+    })
+    return jsonify(game_id)
+
+GAME_KEY_NAMES = {
+    "m": "moves",
+    "n": "name"
+}    
+@app.route('/game/<game_id>', methods=['GET', 'DELETE'])
+def return_game(game_id=None):
+    if request.method == 'GET':
+        game = gamesdb.find_one({"id": game_id})
+        if game is None:
+            return Response(status=404)
+        # formate dictionary names
+        for key, value in GAME_KEY_NAMES.items():
+            game[value] = game[key]
+            del game[key]
+        return json.dumps(game, default=str)
 
 if __name__ == '__main__':
     app.run()
